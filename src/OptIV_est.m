0bb50baae27b0667208d4606040bb3cad6f39552
rtodinkla <r.t.o.dinkla@tudelft.nl>
2025-04-01 15:39:20 +0200

    add work on plotting correlations and visualization of IV-estimates

--- src/OptIV_est.m
+++ src/OptIV_est.m
@@ -199,6 +199,17 @@ plot(u1,'r');
 legend({'$u^{\mathrm{opt}}_{iv}$','ref','$u$'},'Interpreter','latex');
 linkaxes([ax1_1 ax1_2],'x');
 
+ax1_1.LineStyleOrderIndex = ax1_1.LineStyleOrderIndex; % [1]
+ax1_1.LineStyleOrder = {'-o','-+','-*','-x','-s','-d','-v','->','-h','-^'};
+ax1_1.ColorOrder = [1 0 0; 0 1 0; 0 0 1; 0 1 1; 1 0 1];
+hold(ax1_1,'on') % [2]
+ax1_2.LineStyleOrderIndex = ax1_2.LineStyleOrderIndex; % [1]
+ax1_2.LineStyleOrder = ax1_1.LineStyleOrder;
+ax1_2.ColorOrder = ax1_1.ColorOrder;
+hold(ax1_2,'on') % [2]
+% [1] This line forces the axes to behave consistently between releases before and after r2019a
+% [2] Important to preserve the property orders that were just set.
+
 % -------------------- Get system estimate using IV -----------------------
 load('vik.mat');
 num_iter = 2;
@@ -216,60 +227,8 @@ Ziv = [yuRp;yuRf;Yp_r1];
 fro_EfZ(1) = norm(Ef_r1*Ziv.'/N,'fro')^2;
 
 % plotting correlations and estimate of system parameters
-fig10 = figure(10); 
-Fig10FS = 12;
-tl10 = tiledlayout(4+num_iter*1,4,'TileSpacing','compact','Padding','compact');
+plot_corrs;
 
-% Row 1  - plot real system parameters
-nexttile;
-nexttile;
-nexttile;
-imagesc_vik([Up2Yf_inno Uf2Yf_inno],vik,gca);
-nexttile;
-imagesc_vik(Yp2Yf_inno,vik,gca);
-
-% Row 2
-nexttile;
-imagesc_parts(Ef_r1, vik, gca, yuRp, yuRf, Yp_r1);
-title('$\frac{1}{N} E_f \mathcal{Z}^\top$', 'Interpreter', 'latex', 'FontSize', Fig10FS);
-ylabel('$\mathcal{Z}=[R_p; R_f; Y_p]$', 'Interpreter', 'latex', 'FontSize', Fig10FS);
-nexttile;
-imagesc_parts(Wp_r1, vik, gca, yuRp, yuRf, Yp_r1);
-title('$\frac{1}{N} W_p \mathcal{Z}^\top$, $W_p=[U_p; U_f; Y_p]$', 'Interpreter', 'latex', 'FontSize', Fig10FS);
-nexttile; 
-LestZr = Yf_r1 * Ziv.' / N * pinv(Wp_r1 * Ziv.' / N);
-imagesc_vik(LestZr(:,1:end-ny*p), vik, gca);
-title('$\frac{1}{N} Y_f\mathcal{Z}^\top (\frac{1}{N} W_p\mathcal{Z}^\top)^\dagger$, $U_{pf} \rightarrow Y_f$', 'Interpreter', 'latex', 'FontSize', Fig10FS);
-nexttile;
-imagesc_vik(LestZr(:,end-ny*p+1:end), vik, gca);
-title('$\frac{1}{N} Y_f\mathcal{Z}^\top (\frac{1}{N} W_p\mathcal{Z}^\top)^\dagger$, $Y_p \rightarrow Y_f$', 'Interpreter', 'latex', 'FontSize', Fig10FS);
-
-% Row 3
-nexttile;
-imagesc_parts(Ef_r1, vik, gca, Up_r1, Uf_r1, Yp_r1);
-ylabel('$\mathcal{Z}=[U_p; U_f; Y_p]$', 'Interpreter', 'latex', 'FontSize', Fig10FS);
-nexttile;
-imagesc_parts(Wp_r1, vik, gca, Up_r1, Uf_r1, Yp_r1);
-nexttile; 
-LestWp = Yf_r1 * Wp_r1.' / N * pinv(Wp_r1 * Wp_r1.' / N);
-imagesc_vik(LestWp(:,1:end-ny*p), vik, gca);
-nexttile;
-imagesc_vik(LestWp(:,end-ny*p+1:end), vik, gca);
-
-% Row 4
-nexttile;
-imagesc_parts(Ef_r1, vik, gca, Up_r2, Uf_r2, Yp_r2);
-ylabel('$\mathcal{Z}=[\tilde{U}_p; \tilde{U}_f; \tilde{Y}_p]_{\mathrm{opt}}$', 'Interpreter', 'latex', 'FontSize', Fig10FS);
-nexttile;
-imagesc_parts(Wp_r1, vik, gca, Up_r2, Uf_r2, Yp_r2);
-nexttile; 
-LestZopt = Yf_r1 * Ziv_opt.' / N * pinv(Wp_r1 * Ziv_opt.' / N);
-imagesc_vik(LestZopt(:,1:end-ny*p), vik, gca);
-nexttile;
-imagesc_vik(LestZopt(:,end-ny*p+1:end), vik, gca);
-
-Yf_iv = Yf_r1;
-yr_ref = [y_ref;u_ref];
 
 % % Transient Predictor
 % tLest = zeros(ny*f,(p+f)*(nu+ny));
